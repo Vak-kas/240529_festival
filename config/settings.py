ce5bfa3a03ab3dd33f6c20b82ac3418b083623a2
서민재 <hey_minj@naver.com>
2024-05-09 15:24:42 +0900

    모델 설정

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "register.apps.RegisterConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- register/models.py
+++ register/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nickname = models.CharField(max_length=20) #닉네임
+    age = models.IntegerField() #나이
+    instagram_id = models.CharField(max_length = 50) #인스타 아이디
+    #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ#
+    # 추가옵션
+    hobby = models.CharField(max_length=50) #취미 또는 성격
+    sex = models.CharField(max_length=1, choices=[("M", "Male"), ("F", "Female")])  # 성별(M, F 중에 고를 것)
+
+
+
